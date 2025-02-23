19e0a6c35580c4cde0cd3b68e55502253f52a291
aoyama1025 <asuka19901025@gmail.com>
2025-02-23 16:44:49 +0900

    課題7をアップロード

---
+++ kadai_007/Posts_Chapter07.java
@@ -0,0 +1,77 @@
+package kadai_007;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Posts_Chapter07 {
+	public static void main(String[] args) {
+		
+		Connection con = null;
+		Statement statement = null;
+		
+		//投稿データ
+		String[][] posts = {
+				{"1003", "2023-02-08", "昨日の夜は徹夜でした・・", "13"},
+				{"1002", "2023-02-08", "お疲れ様です！", "12"},
+				{"1003", "2023-02-09", "今日も頑張ります！", "18"},
+				{"1001", "2023-02-09", "無理は禁物ですよ！", "17"},
+				{"1002", "2023-02-10", "明日から連休ですね！", "20"},
+		};
+		
+		try {
+			//データベースに接続
+			con =  DriverManager.getConnection(
+	                "jdbc:mysql://localhost/challenge_java",
+	                "root",
+	                "Sugiyuma3!"
+	            );
+			
+			System.out.println("データベース接続成功:" + con);
+			
+			//SQLクエリを準備
+			statement = con.createStatement();
+
+			int totalRows = 0;
+			for (String[] post : posts) {
+				String sql = "INSERT INTO posts (user_id, posted_at, post_content, likes) VALUES ('"
+                        + post[0] + "', '" + post[1] + "', '" + post[2] + "', '" + post[3] + "')";
+				
+				
+				//SQLクエリを実行
+                int rowCnt = statement.executeUpdate(sql);
+                totalRows += rowCnt;
+			}
+				System.out.println("レコード追加を実行します");
+                System.out.println( totalRows + "件のレコードが追加されました");
+			
+                
+                //投稿データを検索する
+                String selectsql ="SELECT posted_at,post_content,likes FROM posts WHERE user_id = 1002;";
+                System.out.println("ユーザーIDが1002のレコードを検索しました");
+                
+                //SQLクエリを実行
+                ResultSet result = statement.executeQuery(selectsql);
+                while(result.next()) {
+                	Date postedAt = result.getDate("posted_at");
+                	String content = result.getString("post_content");
+                	int likes = result.getInt("likes");
+                	System.out.println(result.getRow() + "件目：投稿日時=" + postedAt + "／投稿内容=" + content + "／いいね数=" + likes);
+            }
+		}
+			catch(SQLException e) {
+				System.out.println("エラー発生：" + e.getMessage());
+	        } finally {
+	            // 使用したオブジェクトを解放
+	            if( statement != null ) {
+	                try { statement.close(); } catch(SQLException ignore) {}
+	            }
+	            if( con != null ) {
+	                try { con.close(); } catch(SQLException ignore) {}
+	            }
+			}
+	}
+}
